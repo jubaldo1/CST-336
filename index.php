bd6060ec05af7ace5dd3c1dd90c752912cfeb65a
Jessica May Ubaldo <jubaldo@csumb.edu>
2018-02-07 04:38:52 +0000

    Homework assignment files

--- index.php
+++ index.php
@@ -1,16 +1,9 @@
-<?php<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <title> </title>
     </head>
     <body>
-        //  include_once("index.html"); 
-        <div>
-            <h1>Labs</h1>
-            <!--/ means start at root directory-->
-            <a href="/Labs/Personal_Portfolio">Lab 1</a>
-            <!--no / means "start here", which is still the root directory-->
-            <a href="Labs">Labs</a>
-        </div>
+        <?php include_once("index.html"); ?>
     </body>
-</html>?>
+</html>
