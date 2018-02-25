47fa6613583c547e87a4a66ca2ad440fd8e8893e
Jessica May Ubaldo <jubaldo@csumb.edu>
2018-02-25 09:09:42 +0000

    Added a login-php

---
+++ Homeworks/Homework_2/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Start Page</title>
+    </head>
+    <body>
+        <a href="login.php">Login Here</a>
+    </body>
+</html>
---
+++ Homeworks/Homework_2/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Login Page</title>
+    </head>
+    <body>
+        <form>
+            Username:<br>
+            <input type="text" name=""/><br>
+            Password:<br>
+            <input type="text" name=""/>
+            <input type="submit" value="Submit"/>
+        </form>
+    </body>
+</html>
